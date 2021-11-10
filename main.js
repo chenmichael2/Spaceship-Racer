4d8bf318562785d25343b3dc3fb1817b9822bfb9
chenmichael2 <chenmichael2012@gmail.com>
2021-11-10 12:07:19 -0800

    Game is working

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 let game = document.querySelector('#game-canvas');
 let ctx = game.getContext('2d');
 let play = document.querySelector('#bottom-right');
+let text = document.querySelector('#bottom-center');
+let runGame;
+let playerOneScore = 0;
+let playerTwoScore = 0;
 
 let playerOne;
 let playerTwo;
@@ -13,7 +17,7 @@ window.addEventListener('load', function () {
 });
 
 
-
+    //Objects
 class plane {
     constructor(x1, y1, x2, y2, x3, y3, color) {
         this.x1 = x1; //front
@@ -22,7 +26,8 @@ class plane {
         this.y2 = y2;
         this.x3 = x3; //back right
         this.y3 = y3;
-        this.color = color
+        this.movement = 'stop';
+        this.color = color;
         this.alive = true;
 
         this.render = function () {
@@ -55,141 +60,242 @@ class asteroid {
     }
 }
 
-// 2 Player
+    // 2 Player
 play.addEventListener('click', function start2Player() {
-    //creating ships
+//creating ships
+    clearInterval(runGame);
+    let playerOneScore = 0
+    let playerTwoScore = 0
+    document.querySelector('#score1').textContent = playerOneScore;
+    document.querySelector('#score2').textContent = playerTwoScore;
     arr.length = 0
     playerOne = new plane(145, 500, 140, 530, 150, 530, 'white');
     playerOne.render();
     playerTwo = new plane(442, 500, 437, 530, 447, 530, 'gray');
     playerTwo.render();
-    //movement of players
-    document.addEventListener('keypress', playersMove);
-    function playersMove(e) {
-        console.log('movement', e.key);
 
-        switch (e.key) {
-            case 'w': //move up
-                playerOne.y1 >= 0 ? playerOne.y1 -= 3 : playerOne.y1 = 0;
-                playerOne.y2 >= 30 ? playerOne.y2 -= 3 : playerOne.y2 = 30;
-                playerOne.y3 >= 30 ? playerOne.y3 -= 3 : playerOne.y3 = 30;
+//movement of players
+    document.addEventListener('keypress', playerMovement);
+    document.addEventListener('keyup', playerStop);
+    
+//asteroid
+    createAsteroid();
+
+//Collision
+    collisionPlayerOne();
+    collisionPlayerTwo();
+    playerOneWin();
+    playerTwoWin();
+        
+//Win Conditions
+    function playerOneWin() {
+        if (playerOne.y1 < 1) {
+            resetPlayerOne();
+            document.querySelector('#score1').textContent = playerOneScore += 1
+        }
+        return;
+    }
+    function playerTwoWin() {
+        if (playerTwo.y1 < 1) {
+            resetPlayerTwo();
+            document.querySelector('#score2').textContent = playerTwoScore += 1
+        }
+        return;
+    }
+//Player Won
+    function playerOneWon() {
+        if (playerOneScore === 5) {
+            text.textContent = "Player One Won"
+        }
+        return
+    }
+    
+        runGame = setInterval(gameLoop, 1);
+
+        function gameLoop() {
+            ctx.clearRect(0, 0, game.width, game.height);
+            playerTwo.render();
+            playerOne.render();
+            asteroidRender();
+            asteroidMove();
+            collisionPlayerOne();
+            collisionPlayerTwo();
+            player1Moving();
+            player2Moving();
+            playerOneWin();
+            playerTwoWin();
+            playerOneWon();
+        }
+    });
+
+
+    /////Functions!
+
+// RESET PLAYER
+    function resetPlayerOne() {
+        playerOne.x1 = 145;
+        playerOne.y1 = 500;
+        playerOne.x2 = 140;
+        playerOne.y2 = 530;
+        playerOne.x3 = 150;
+        playerOne.y3 = 530;
+    }
+    function resetPlayerTwo() {
+        playerTwo.x1 = 442;
+        playerTwo.y1 = 500;
+        playerTwo.x2 = 437;
+        playerTwo.y2 = 530,
+        playerTwo.x3 = 447;
+        playerTwo.y3 = 530;
+    }
+
+//asteroid rendering
+    function asteroidRender() {
+        for (let i = 0; i < arr.length; i++) {
+            arr[i].render();
+        }
+    }
+
+//asteroid movement
+    function asteroidMove() {
+        for (let i = 0; i < arr.length; i++) {
+            switch (arr[i].direction) {
+                case 'r':
+                    if (arr[i].x + 3 > 590) {
+                        arr[i].direction = 'l'
+                    } else {
+                        arr[i].x += (Math.floor(Math.random() * 3));
+                    }
+                    break;
+                case 'l':
+                    if (arr[i].x - 3 < 0) {
+                        arr[i].direction = 'r'
+                    } else {
+                        arr[i].x -= (Math.floor(Math.random() * 3));
+                    }
+                    break;
+            }
+        }
+    }
+
+//player movement
+    function playerMovement (e) {
+        if (e.key == 'w') {
+            playerOne.movement = 'up';
+            console.log(playerOne.movement);
+        }
+        if (e.key == 's') {
+            playerOne.movement = 'down';
+            console.log(playerOne.movement);
+        }
+        if (e.key == 'i') {
+            playerTwo.movement = 'up';
+            console.log(playerTwo.movement);
+        }
+        if (e.key == 'k') {
+            playerTwo.movement = 'down';
+            console.log(playerTwo.movement)
+        }
+    }
+    function playerStop (e) {
+        if (e.key == 'w') {
+            playerOne.movement = 'stop';
+            console.log(playerOne.movement);
+        }
+        if (e.key == 's') {
+            playerOne.movement = 'stop';
+            console.log(playerOne.movement);
+        }
+        if (e.key == 'i') {
+            playerTwo.movement = 'stop';
+            console.log(playerTwo.movement);
+        }
+        if (e.key == 'k') {
+            playerTwo.movement = 'stop';
+            console.log(playerTwo.movement);
+        }
+    }
+    function player1Moving() {
+        switch (playerOne.movement) {
+            case 'up':
+                playerOne.y1 >= 0 ? playerOne.y1 -= 1 : playerOne.y1 = 0;
+                playerOne.y2 >= 30 ? playerOne.y2 -= 1 : playerOne.y2 = 30;
+                playerOne.y3 >= 30 ? playerOne.y3 -= 1 : playerOne.y3 = 30;
                 break;
-            case 's': //move down
-                playerOne.y1 <= 500 ? playerOne.y1 += 3 : playerOne.y1 = 500;
-                playerOne.y2 <= 530 ? playerOne.y2 += 3 : playerOne.y2 = 530;
-                playerOne.y3 <= 530 ? playerOne.y3 += 3 : playerOne.y3 = 530;
+            case 'stop':
+                playerOne.y1 -= 0;
+                playerOne.y2 -= 0;
+                playerOne.y3 -= 0;
+                break;   
+            case 'down':
+                playerOne.y1 <= 500 ? playerOne.y1 += 1 : playerOne.y1 = 500;
+                playerOne.y2 <= 530 ? playerOne.y2 += 1 : playerOne.y2 = 530;
+                playerOne.y3 <= 530 ? playerOne.y3 += 1 : playerOne.y3 = 530;
                 break;
-            case 'i': //move up
-                playerTwo.y1 >= 0 ? playerTwo.y1 -= 3 : playerTwo.y1 = 0;
-                playerTwo.y2 >= 30 ? playerTwo.y2 -= 3 : playerTwo.y2 = 30;
-                playerTwo.y3 >= 30 ? playerTwo.y3 -= 3 : playerTwo.y3 = 30;
-                break;
-            case 'k': //move down
-                playerTwo.y1 <= 500 ? playerTwo.y1 += 3 : playerTwo.y1 = 500;
-                playerTwo.y2 <= 530 ? playerTwo.y2 += 3 : playerTwo.y2 = 530;
-                playerTwo.y3 <= 530 ? playerTwo.y3 += 3 : playerTwo.y3 = 530;
-                break;
-        };
+        } 
     };
-    //asteroid
+    function player2Moving() {
+        switch (playerTwo.movement) {
+            case 'up':
+                playerTwo.y1 >= 0 ? playerTwo.y1 -= 1 : playerTwo.y1 = 0;
+                playerTwo.y2 >= 30 ? playerTwo.y2 -= 1 : playerTwo.y2 = 30;
+                playerTwo.y3 >= 30 ? playerTwo.y3 -= 1 : playerTwo.y3 = 30;
+                break;
+            case 'down':
+                playerTwo.y1 <= 500 ? playerTwo.y1 += 1 : playerTwo.y1 = 500;
+                playerTwo.y2 <= 530 ? playerTwo.y2 += 1 : playerTwo.y2 = 530;
+                playerTwo.y3 <= 530 ? playerTwo.y3 += 1 : playerTwo.y3 = 530;
+                break;
+            case 'stop':
+                playerTwo.y1 -= 0;
+                playerTwo.y2 -= 0;
+                playerTwo.y3 -= 0;
+                break;
+        } 
+    };
+
+//Creating Asteroid
     function createAsteroid() {
         for (let i = 0; i < 20; i++) {
-            let ast = new asteroid(Math.floor(Math.random() * 590), Math.floor(Math.random() * 500), 3);
+            let ast = new asteroid(Math.floor(Math.random() * 590), Math.floor(Math.random() * 420) + 30, 3);
             if (i % 2 === 0) {
                 ast.direction = 'l'
             }
             arr.push(ast);
-            
+
             // arr[i].render();
         }
     };
-    createAsteroid();
-//Collision
-// let hitTest = (
-//     p1.y + p1.height > p2.y && 
-//     p1.y < p2.y + p2.height &&
-//     p1.x + p1.width > p2.x &&
-//     p1.x < p2.x + p2.width
-// ); // {boolean} : if all are true -> hit
 
-// if (hitTest) {
-//     return addNewShrek();
-// } else {
-//     return false;
-// }
-// };
-
+//Collision Functions
     function collisionPlayerOne() {
         for (let i = 0; i < arr.length; i++) {
             let hitTest = (
-                playerOne.y2 + 30 >  
+                playerOne.x2 < arr[i].x + 6 &&
+                playerOne.x3 > arr[i].x - 6 &&
+                playerOne.y1 < arr[i].y + 6 &&
+                playerOne.y2 > arr[i].y - 6
             )
+            if (hitTest) {
+                resetPlayerOne();
+            }
+        } 
+        return false;
+    }
+
+    function collisionPlayerTwo() {
+        for (let i = 0; i < arr.length; i++) {
+            let hitTest = (
+                playerTwo.x2 < arr[i].x + 6 &&
+                playerTwo.x3 > arr[i].x - 6 &&
+                playerTwo.y1 < arr[i].y + 6 &&
+                playerTwo.y2 > arr[i].y - 6
+            )
+            if (hitTest) {
+                resetPlayerTwo();
+            }
         }
+        return false;
     }
+        
     
-
-    collisionPlayerOne();
-    
-
-    
-
-    //Win
-
-    const runGame = setInterval(gameLoop, 1);
-
-function gameLoop() {
-    ctx.clearRect(0, 0, game.width, game.height);
-    playerTwo.render();
-    playerOne.render();
-    asteroidRender();
-    asteroidMove();
-    collisionPlayerOne();
-}
-});
-
-
-/////Functions!
-
-// RESET PLAYER
-function resetPlayerOne() {
-    playerOne.x1 = 145;
-    playerOne.y1 = 500;
-    playerOne.x2 = 140;
-    playerOne.y2 = 530;
-    playerOne.x3 = 150;
-    playerOne.y3 = 530;
-}
-function resetPlayerTwo() {
-    playerTwo.x1 = 442;
-    playerTwo.y1 = 500; 
-    playerTwo.x2 = 437;
-    playerTwo.y2 = 530,
-    playerTwo.x3 = 447;
-    playerTwo.y3 = 530;
-}
-//asteroid rendering
-function asteroidRender() {
-    for (let i = 0; i < arr.length; i++) {
-        arr[i].render();
-    }
-}
-//asteroid movement
-function asteroidMove() {
-    for (let i = 0; i < arr.length; i++) {
-        switch (arr[i].direction) {
-            case 'r':
-                if (arr[i].x + 3 > 590) {
-                    arr[i].direction = 'l'
-                } else {
-                    arr[i].x += (Math.floor(Math.random() * 3));
-                }
-                break;
-            case 'l':
-                if (arr[i].x - 3 < 0) {
-                    arr[i].direction = 'r'
-                } else {
-                    arr[i].x -= (Math.floor(Math.random() * 3));
-                }
-        }
-    }
-}
